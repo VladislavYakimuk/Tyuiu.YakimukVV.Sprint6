543eca347b68540586effc2b5218156ee82ce8ab
Владислав Якимук <Tyuiu.YakimukVV@outlook.com>
2024-12-12 20:35:31 +0500

    Task7

--- Tyuiu.YakimukVV.Sprint6.Task7.V13.Lib/DataService.cs
+++ Tyuiu.YakimukVV.Sprint6.Task7.V13.Lib/DataService.cs
@@ -35,7 +35,7 @@ namespace Tyuiu.YakimukVV.Sprint6.Task7.V13.Lib
 
             for (int r = 0; r < rows_1; r++)
             {
-                for (int c = 6; c <= 6; c++)
+                for (int c = 7; c <= 7; c++)
                 {
                     if ((array[r, c] > 0) && (array[r, c] % 2 == 0))
                     {
